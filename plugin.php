5702832c1830f838c3a49ec25f51f0b5aecacb14
Free-counter <Free-counter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-01 10:03:13 +0000

    added comments and fixed bugs
    git-svn-id: https://plugins.svn.wordpress.org/free-counter/trunk@1125224 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -728,6 +728,7 @@
     if(is_admin()) {
         // check server - local or local network
         add_action('admin_notices', array('counter_free_plagin', 'notices'));
+        // activate plugin (set default setting)
         register_activation_hook( __FILE__, array('counter_free_plagin','on_activate'));
         register_deactivation_hook( __FILE__, array('counter_free_plagin','on_deactivate'));
         add_action('admin_menu', array('counter_free_plagin', 'draw_menu'));
