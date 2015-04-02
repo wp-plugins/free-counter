de197a7e9ac6f4b0a9342c48c6eda9c2ff4aa6da
Free-counter <Free-counter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-02 06:53:04 +0000

    added comments and fixed bugs
    git-svn-id: https://plugins.svn.wordpress.org/free-counter/tags/1.1@1126049 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -738,6 +738,7 @@
         add_action('admin_post_exportToCsv', array('counter_free_plagin', 'exportToCsv') );
         // save account
         add_action('admin_post_save_account', array('counter_free_plagin', 'save_account') );
+        // activate plugn(set default setting)
         add_action('admin_post_activate_plugin', array('counter_free_plagin', 'activate_plugin') );
         add_action('wp_ajax_nopriv_check_stat', array('counter_free_plagin', 'check_stat') );   
         add_action('admin_print_styles', "adding_files_style" );   
