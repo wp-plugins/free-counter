a08a3f9a12926b1e47efaf22bbff2ca9307b2608
Free-counter <Free-counter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-04 13:37:05 +0000

    added comments and fixed bugs
    git-svn-id: https://plugins.svn.wordpress.org/free-counter/tags/1.1@1127727 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- template/counter.js
+++ template/counter.js
@@ -112,6 +112,9 @@ function verificationFrom()
     }
     
 }
+/**
+* validate Email
+*/
 function checkmail(value) {
     reg = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
     if (!value.match(reg)) {
